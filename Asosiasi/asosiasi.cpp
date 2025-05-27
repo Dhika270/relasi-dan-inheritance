ced9d1495d68dd7f88990b75441cd53828fe521f
Dhika270 <andikasetiawan05ds@gmail.com>
2025-05-27 16:01:39 +0700

    progres 17

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -61,3 +61,12 @@ void dokter::cetakPasien() {
     }
     cout << endl;
 }
+
+int main() {
+    dokter* varDokter1 = new dokter("dr.Budi");
+    dokter* varDokter2 = new dokter("dr.Tono");
+    dokter* varPasien1 = new dokter("Andi");
+    dokter* varPasien2 = new dokter("Lia");
+
+    
+}
