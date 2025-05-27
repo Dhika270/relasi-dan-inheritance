dec89b8534f45c0f6b31a4d63086bea5f183f1a1
Dhika270 <andikasetiawan05ds@gmail.com>
2025-05-27 15:59:14 +0700

    progres 16

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -54,3 +54,10 @@ void dokter:: tambahanPasien(pasien* pPasien) {
     daftar_pasien.push_back(pPasien);
     pPasien->tambahanDokter(this);
 }
+void dokter::cetakPasien() {
+    cout << "Daftar pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto& a : daftar_pasien) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
