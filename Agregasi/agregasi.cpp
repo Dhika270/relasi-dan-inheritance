49d702cc160d93ab9610233f7e79eee65d0d60ff
Dhika270 <andikasetiawan05ds@gmail.com>
2025-05-27 16:46:20 +0700

    progres 24

--- Agregasi/agregasi.cpp
+++ Agregasi/agregasi.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-#include "Anak.H"
-#include "ibu.H"
+#include "anak.h"
+#include "ibu.h"
 
-int main() {
+int main() { // agregasi
     ibu* varIbu = new ibu("dini");
-    ibu* varIbu2 = new ibu("novi");
+    ibu* varibu2 = new ibu("novi");
     anak* varAnak1 = new anak("tono");
     anak* varAnak2 = new anak("rini");
-    anak* varAnak3 = new anak("dewi");
-    
+    anak* varAnak3 = new anak("Dewi");
+
     varIbu->tambahAnak(varAnak1);
     varIbu->tambahAnak(varAnak2);
     varibu2->tambahAnak(varAnak3);
-    varIbu2->tambahAnak(varAnak1);
+    varibu2->tambahAnak(varAnak1);
 
     varIbu->cetakAnak();
-    varIbu2->cetakAnak();
+    varibu2->cetakAnak();
 
     delete varIbu;
-    delete varIbu2;
+    delete varibu2;
     delete varAnak1;
     delete varAnak2;
     delete varAnak3;
+
+    system("pause");
+
+    return 0;
 }
